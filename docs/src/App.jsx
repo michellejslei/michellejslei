3d903b6c7081e1cf68125cc8d6c50bb1c6aa3cab
Michelle <michellejslei@gmail.com>
2024-07-05 00:51:46 -0700

    pixel loadscreen added

--- docs/src/App.jsx
+++ docs/src/App.jsx
@@ -1,29 +1,43 @@
+import { useState } from 'react';
+
 import styles from './App.module.css'
 import About from './components/About/About'
 import Connect from './components/Connect/Connect'
 import Experience from './components/Experience/Experience'
 import Footer from './components/Footer/Footer'
 import Hero from './components/Hero/Hero'
+import LoadingScreen from './components/LoadingScreen/LoadingScreen'
 import Navbar from './components/Navbar/Navbar'
 import Projects from './components/Projects/Projects'
 import SideQuests from './components/SideQuests/SideQuests'
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  const handleLoaded = () => {
+    setLoading(false);
+  };
 
   return (
     <div className={styles.App}>
-      <Navbar />
-      <div className={styles.main}>
-        <Hero />
-        <About />
-        <Experience />
-        <Projects />
-        {/* <SideQuests /> */}
-        <Connect />
-      </div>
-      <Footer />
+      {loading ? (
+        <LoadingScreen onLoaded={handleLoaded} />
+      ) : (
+        <>
+          <Navbar />
+          <div className={styles.main}>
+            <Hero />
+            <About />
+            <Experience />
+            <Projects />
+            {/* <SideQuests /> */}
+            <Connect />
+          </div>
+          <Footer />
+        </>
+      )}
     </div>
-  )
+  );
 }
 
 export default App
